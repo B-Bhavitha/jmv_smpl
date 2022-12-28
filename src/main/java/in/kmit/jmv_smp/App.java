06226b5190b792c4b55301ce6aa99f50c95f4431
Bhavitha <bhavithabiyyala18@gmail.com>
2022-12-28 23:46:34 +0530

    jkfnb

---
+++ src/main/java/in/kmit/jmv_smp/App.java
@@ -0,0 +1,13 @@
+package in.kmit.jmv_smp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello " );
+    }
+}
